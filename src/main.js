e3bcb0f2621ff5b349e1f018d1b0cefb344642d0
Davi Santos <brisolo32@outlook.com>
2022-10-25 13:28:03 -0300

    Made it buildable and added a cache to ease other programs using it

--- src/main.js
+++ src/main.js
@@ -1,27 +1,50 @@
 import { load } from "cheerio"
 import getUrls from 'get-urls'
 import fetch from 'node-fetch'
+import fs from 'node:fs'
 
 main()
 
-async function scrapeMagnets(url) {
-    const urls = Array.from(getUrls(url))
+async function scrapeMagnets(url, cacheLoc) {
+    if (!fs.existsSync(cacheLoc)) {
+        const urls = Array.from(getUrls(url))
 
-    const requests = urls.map(async url => {
-        const res = await fetch(url)
-        const html = await res.text();
-
-        const $ = load(html)
-
-        return {
-            url,
-            name: $('.title').first().text(),
-            fileSize: $('p[style="text-align: center;"]').children('em').text().replaceAll("File Size: ", ""),
-            magnet: $('.nv-content-wrap p a[href*="magnet:?"]').attr('href')
-        }
-    })
+        const requests = urls.map(async url => {
+            const res = await fetch(url)
+            const html = await res.text();
     
-    return Promise.all(requests)
+            const $ = load(html)
+            const data = {
+                "requests": [
+                    {
+                        name: $('.title').first().text(),
+                        fileSize: $('p[style="text-align: center;"]').children('em').text().replace("File Size: ", ""),
+                        magnet: $('.nv-content-wrap p a[href*="magnet:?"]').attr('href')
+                    }
+                ]
+            }
+    
+            fs.writeFileSync(cacheLoc, JSON.stringify(data))
+        })
+    } else {
+        const urls = Array.from(getUrls(url))
+        const requests = urls.map(async url => {
+            const existingFile = JSON.parse(fs.readFileSync(cacheLoc))
+
+            const res = await fetch(url)
+            const html = await res.text();
+    
+            const $ = load(html)
+            const data = {
+                name: $('.title').first().text(),
+                fileSize: $('p[style="text-align: center;"]').children('em').text().replace("File Size: ", ""),
+                magnet: $('.nv-content-wrap p a[href*="magnet:?"]').attr('href')
+            }
+
+            existingFile.requests.push(data)
+            fs.writeFileSync(cacheLoc, JSON.stringify(existingFile))
+        })
+    }
 }
 
 function main() {
@@ -31,10 +54,10 @@ function main() {
         .split(' ').join('-')
         .replace("\'", "")
     
-    console.log(query)
+    const cache = process.argv[3]
+        .replace("--cache=", "")
 
-    var res = scrapeMagnets(`https://freelinuxpcgames.com/${query}`)
-    res.then(data => {
-        console.log(data)
-    })
+    console.log(`Query: ${query}\nCache Location: ${cache}`)
+
+    scrapeMagnets(`https://freelinuxpcgames.com/${query}`, cache)
 }
